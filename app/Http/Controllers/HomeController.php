1acb168fc35f6e45519540c19360cec1fa6aa97e
Raphael Cunha <rbcunhadesign@gmail.com>
2019-08-13 14:24:58 -0400

    Change method name to __invoke and call Bucket controller class

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,22 +7,12 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function __invoke()
     {
-        return view('home');
+        return app()->call('App\Http\Controllers\ExpenseReportBucketController@index');
     }
 }
