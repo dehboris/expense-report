7f75f8ce23c52a08c00c876d5f320cc24fd66ebe
Raphael Cunha <rbcunhadesign@gmail.com>
2019-08-15 16:22:11 -0400

    Set up Bucket routes

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,29 @@
 <?php
 
+use App\Http\Controllers\ExpenseReport\BucketsController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * API Routes
+ */
+Route::middleware('auth')->group(function() {
+    /**
+     * Expense Report Routes
+     * @routeName expense-report
+     */
+    Route::as('expense-report.')->group(function() {
+        /**
+         * Expense Report Bucket Routes
+         * @routeName buckets.
+         * @routePrefix buckets/
+         */
+        Route::as('buckets.')->group(function() {
+            Route::get('', [BucketsController::class, 'index'])->name('index');
+            Route::post('', [BucketsController::class, 'store'])->name('store');
+            Route::get('{bucket}', [BucketsController::class, 'show'])->name('show');
+            Route::patch('{bucket}', [BucketsController::class, 'update'])->name('update');
+            Route::delete('{bucket}', [BucketsController::class, 'destroy'])->name('destroy');
+        });
+    });
 });
