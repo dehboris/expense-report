ad2560e45da1e9e3698c13e62379726774e4fd83
Raphael Cunha <rbcunhadesign@gmail.com>
2019-08-15 15:29:20 -0400

    Changing form spacing

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -13,15 +13,15 @@
 
                 <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
 
-                    <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
+                    <div class="font-semibold bg-gray-200 text-gray-700 py-2 px-4 mb-0">
                         {{ __('Reset Password') }}
                     </div>
 
-                    <form class="w-full p-6" method="POST" action="{{ route('password.email') }}">
+                    <form class="w-full py-4 px-6" method="POST" action="{{ route('password.email') }}">
                         @csrf
 
-                        <div class="flex flex-wrap mb-6">
-                            <label for="email" class="block text-gray-700 text-sm font-bold mb-2">
+                        <div class="flex flex-wrap mb-4">
+                            <label for="email" class="block text-gray-700 text-sm font-bold mb-1">
                                 {{ __('E-Mail Address') }}:
                             </label>
 
@@ -35,7 +35,7 @@
                         </div>
 
                         <div class="flex flex-wrap">
-                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-gray-100 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-gray-100 font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline">
                                 {{ __('Send Password Reset Link') }}
                             </button>
 
