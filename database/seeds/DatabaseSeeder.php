6fee08c0e43d58f0a701d10608a3b11f7741e5e1
Raphael Cunha <rbcunhadesign@gmail.com>
2019-08-15 14:08:45 -0400

    Add ability to scaffold large amount of data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\ExpenseReport\Bucket;
+use App\ExpenseReport\BucketItem;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(UsersTableSeeder::class);
+
+        factory(User::class, 1000)->create()->each(function($user) {
+            factory(Bucket::class, rand(3, 5))->make(['user_id' => $user->id])->each(function($bucket) use ($user) {
+                $user->buckets()->save($bucket);
+                factory(BucketItem::class, rand(50, 100))->make(['bucket_id' => $bucket->id])->each(function($item) use ($bucket) {
+                    $bucket->items()->save($item);
+                });
+            });
+        });
     }
 }
