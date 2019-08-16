3a5b5ec2a9c41198fccdbc99bece050091a88911
Raphael Cunha <rbcunhadesign@gmail.com>
2019-08-16 16:59:32 -0400

    Add Ziggy to project

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,12 +6,19 @@ import { routes } from './routes';
 
 window.Vue = require('vue');
 
+Vue.mixin({
+    methods: {
+        route: route
+    }
+});
+
 /*#######################################################
  # Vue Router Settings
  ######################################################*/
 Vue.use(VueRouter);
 
 const router = new VueRouter({
+    mode: 'history',
     routes // short for `routes: routes`
 });
 
