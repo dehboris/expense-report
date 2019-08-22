416eb837f1eec292602e8c319d1a22372368eed7
Raphael Cunha <rbcunhadesign@gmail.com>
2019-08-21 23:14:56 -0400

    Better align form on mobile view

--- resources/js/components/Buckets/Bucket.vue
+++ resources/js/components/Buckets/Bucket.vue
@@ -20,20 +20,20 @@
             <p v-if="form.errors.name !== undefined" class="text-xs text-red-600 mt-1">{{ form.errors.name[0] }}</p>
         </div>
 
-        <div class="w-full mb-6 md:w-2/12 md:px-3 md:mb-0 xl:w-3/12">
+        <div class="w-1/2 pr-2 mb-6 md:w-2/12 md:px-3 md:mb-0 xl:w-3/12">
           <label for="amount" class="block font-semibold">Amount:</label>
           <input type="text" id="amount" :class="['input', { 'border-red-500': form.errors.amount !== undefined }]" placeholder="245.99" v-model="form.amount">
 
           <p v-if="form.errors.amount !== undefined" class="text-xs text-red-600 mt-1">{{ form.errors.amount[0] }}</p>
         </div>
 
-        <div class="w-full flex flex-wrap self-center mb-6 md:w-2/12 md:px-3 md:mb-0">
-          <label for="credit" class="w-full block">
+        <div class="w-1/2 pl-2 flex flex-wrap self-end sm:self-center mb-6 md:w-2/12 md:px-3 md:mb-0">
+          <label for="credit" class="w-1/2 block">
             <input type="radio" name="type" id="credit" value="credit" v-model="form.type">
             Credit
           </label>
 
-          <label for="debit" class="w-full block">
+          <label for="debit" class="w-1/2 block">
             <input type="radio" name="type" id="debit" value="debit" v-model="form.type">
             Debit
           </label>
